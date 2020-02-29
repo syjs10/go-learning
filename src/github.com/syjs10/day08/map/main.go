c3d1cf16cedf4f3021ca60083a3b692792d07508
JS <350172898@qq.com>
2020-02-29 20:54:11 +0800

    Create main.go

---
+++ src/github.com/syjs10/day08/map/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string]int{
+		"test": 1,
+		"abc":  2,
+	}
+	y := map[string]int{
+		"test": 1,
+		"abc":  3,
+	}
+	fmt.Println(equal(x, y))
+}
+
+func equal(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
