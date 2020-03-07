8b3519952dd614fd4b0c6fb8fae6ac9c24486330
JS <350172898@qq.com>
2020-03-07 23:36:30 +0800

    Create main.go

---
+++ src/github.com/syjs10/day10/structtest/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+
+// Employee 员工
+type Employee struct {
+	ID        int
+	Name      string
+	Address   string
+	DoB       time.Time
+	Position  string
+	Salary    int
+	ManagerID int
+}
+
+var dilbert Employee
