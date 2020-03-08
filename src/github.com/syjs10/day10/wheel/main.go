26f6f99fcb80d59c3e5d18806dfdc6bfa6c211a5
JS <350172898@qq.com>
2020-03-09 01:11:35 +0800

    Create main.go

---
+++ src/github.com/syjs10/day10/wheel/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// Point 点
+type Point struct {
+	X, Y int
+}
+
+// Circle 圆
+type Circle struct {
+	Center Point
+	Radius int
+}
+
+// Wheel 轮子
+type Wheel struct {
+	Circle Circle
+	Spokes int
+}
+
+func main() {
+	var w Wheel
+	w.Circle.Center.X = 8
+	w.Circle.Center.Y = 8
+	w.Circle.Radius = 5
+	w.Spokes = 20
+	w = Wheel{Circle{Point{8, 8}, 5}, 20}
+	w = Wheel{
+		Circle: Circle{
+			Center: Point{
+				X: 8,
+				Y: 8,
+			},
+			Radius: 5,
+		},
+		Spokes: 20,
+	}
+	fmt.Printf("%#v\n", w)
+}
