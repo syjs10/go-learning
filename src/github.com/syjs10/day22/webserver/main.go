f2d0c9f405ab8c9cb798492c4146a69563467ae4
JS <350172898@qq.com>
2020-05-05 00:10:11 +0800

    Create main.go

---
+++ src/github.com/syjs10/day22/webserver/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"log"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, "Hello!")
+}
+
+func main() {
+	var h Hello
+	err := http.ListenAndServe("localhost:4000", h)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
